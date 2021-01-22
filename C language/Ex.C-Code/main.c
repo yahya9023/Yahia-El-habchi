4bc58c81ad6e673061d8e23a288e64730b04ca99
yahya <yahyaelhabchi476@gmail.com>
2021-01-22 10:11:13 +0100

    add C lang Ex

---
+++ C language/Ex.C-Code/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+    char name[10],lastname[10],sexe[8],num[20];
+    int age;
+
+
+    printf("My name is :");
+    scanf("%s",&name);
+
+    printf("My last name is :");
+    scanf("%s",&lastname);
+
+    printf("My sexe is :");
+    scanf("%s",&sexe);
+
+    printf("My age is :");
+    scanf("%d",&age);
+
+    printf("My phone number is :");
+    scanf("%s",&num);
+
+    printf("My name is %s, My last name is %s, my sexe is %s, my age is %d, my number phone is %s",name,lastname,sexe,age,num);
+
+
+    int num1,num2;
+
+    printf("The first number is :");
+    scanf("%d",&num1);
+
+    printf("The second number is :");
+    scanf("%d",&num2);
+
+    printf("num1+num2= %d \n",num1+num2);
+    printf("num1-num2= %d \n",num1-num2);
+    printf("num1*num2= %d \n",num1*num2);
+    printf("num1/num2= %d \n",num1/num2);
+    printf("num1%num2= %d \n",num1 % num2);
+
+
+    double num1;
+    double num2;
+    char op;
+
+    printf("Entre a number: ");
+    scanf("%lf",&num1);
+    printf("Entre operator: ");
+    scanf(" %c",&op);
+    printf("Entre a number: ");
+    scanf("%lf",&num2);
+
+    if(op == '+'){
+        printf("%.2f",num1+num2);
+    }
+    else if (op == '-'){
+        printf("%.2f",num1-num2);
+    }
+    else if (op == '/') {
+        printf("%.2f",num1/num2);
+    }
+    else if (op == '*'){
+        printf("%.2f", (num1 * num2));
+    }
+    else{
+        printf("Invalid Operator");
+    }
+
+*/
+
+    int numberOne;
+    int numberTwo;
+    printf("enter first number :");
+    scanf("%i",&numberOne);
+    printf("enter second number :");
+    scanf("%i",&numberTwo);
+    printf("first number is : %o Octet and %x Hexadecimal\n",numberOne,numberOne);
+    printf("second number is :%o Octet and %x Hexadecimal\n",numberTwo,numberTwo);
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
